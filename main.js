741812fc6fa2f6bc8515ad849b0be0bd34ae27e7
Your Name <239422226+ozzzzzzzz27@users.noreply.github.com>
2025-10-21 20:27:09 +0100

    add markers + popup

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const map = new maplibregl.Map({
 	container: 'map',
 	style: 'https://tiles.openfreemap.org/styles/liberty',
-	center: [0, 0],  // choose your view
-	zoom: 2
+	center: [0, 0],
+	zoom: 2,
+	attributionControl: false
 });
 
-map.addControl(new maplibregl.NavigationControl())
+map.addControl(new maplibregl.NavigationControl());
+
+// When user clicks on the map
+map.on('click', (e) => {
+	const { lng, lat } = e.lngLat;
+
+	// Create a popup
+	const popup = new maplibregl.Popup({ offset: 25 })
+		.setHTML('<h4>Hello world</h4>');
+
+	// Create and add the marker
+	new maplibregl.Marker({ color: 'red' })
+		.setLngLat([lng, lat])
+		.setPopup(popup)
+		.addTo(map);
+});
